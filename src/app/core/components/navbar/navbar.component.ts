fc29a6adcefb7eedf83d313e72db82a542af6c77
luan goedert <luan_goedert2013@hotmail.com>
2025-11-13 19:03:35 -0300

    re adding footer component, deleting unused component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, } from "@angular/router";
-import { PortraitComponent } from '../portrait/portrait.component';
 import { DetailsComponent } from '../details/details.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,                // <-- important
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [RouterLink, PortraitComponent, DetailsComponent],
+  imports: [RouterLink, DetailsComponent, DatePipe],
 })
+
+
 export class NavbarComponent {
+
+  programmingSoftwareSince = new Date('2020-09-08');
+  programmingGamesSince = new Date('2023-01-10');
+  gameDeveloperXp = this.getDaysAsXP(this.programmingGamesSince);
+  softwareDeveloperXP = this.getDaysAsXP(this.programmingSoftwareSince);
+  myAge = this.getAge('1996-11-18');
+  gamesDelivered = 1;
+  email = 'luangoedert336@gmail.com';
+  location = 'Brazil';
 
   scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
@@ -19,4 +30,28 @@ export class NavbarComponent {
     }
   }
 
+  getDaysAsXP(startingDate: Date): number {
+    const today = new Date();
+    const diffms = today.getTime() - startingDate.getTime();
+    const miliSecondsPerSecond = 1000;
+    const secondsPerMinute = 60;
+    const minutesPerHour = 60;
+    const hoursPerday = 24
+
+    return Math.floor(diffms / (miliSecondsPerSecond * secondsPerMinute * minutesPerHour * hoursPerday));
+  }
+
+  getAge(birthdate: string): number {
+    const today = new Date();
+    const birthday = new Date(birthdate);
+
+    let age = today.getFullYear() - birthday.getFullYear();
+    const monthDiff = today.getMonth() - birthday.getMonth();
+
+    // If birthday hasn't happened yet this year, subtract 1
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
--- src/app/core/components/portrait/portrait.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-portrait',
-  templateUrl: './portrait.component.html',
-  styleUrls: ['./portrait.component.scss'],
-})
-export class PortraitComponent {}
